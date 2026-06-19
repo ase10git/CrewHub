12044980749622a010d6f196289a3653455cff7e
ase10git <ase1031@naver.com>
2026-06-19 11:35:55 +0900

    chore: SpringBoot 프로젝트 추가

---
+++ backend/src/test/java/io/github/crewhub/CrewhubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.crewhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrewhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
