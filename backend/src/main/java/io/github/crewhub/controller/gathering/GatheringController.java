a7a5535ff26f7585b4edaef8726ee00ac52c38d9
ase10git <ase1031@naver.com>
2026-06-20 11:09:58 +0900

    feat: 모임 생성 API 추가(#7)

--- backend/src/main/java/io/github/crewhub/controller/gathering/GatheringController.java
+++ backend/src/main/java/io/github/crewhub/controller/gathering/GatheringController.java
@@ -1,6 +1,15 @@
 package io.github.crewhub.controller.gathering;
 
+import io.github.crewhub.common.response.ApiResponse;
+import io.github.crewhub.dto.gathering.request.CreateGatheringRequest;
+import io.github.crewhub.dto.gathering.response.CreateGatheringResponse;
+import io.github.crewhub.security.details.CustomUserDetails;
+import io.github.crewhub.service.gathering.GatheringService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/gathering")
 @RequiredArgsConstructor
 public class GatheringController {
-    // 생성, 조회, 상세보기, 수정, 삭제
+    private final GatheringService gatheringService;
 
+    @PostMapping
+    public ApiResponse<CreateGatheringResponse> create(
+            @Valid @RequestBody CreateGatheringRequest request,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+            ) {
+        return ApiResponse.success(gatheringService.create(userDetails.getUserId(), request));
+    }
 }
