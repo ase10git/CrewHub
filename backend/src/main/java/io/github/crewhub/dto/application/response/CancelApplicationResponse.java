932b3aed22fa05c1c7529a3ceb3b535cd27347e8
ase10git <ase1031@naver.com>
2026-06-22 07:29:29 +0900

    fix: 지원서 취소 응답 레코드 이름 수정(#8)

--- backend/src/main/java/io/github/crewhub/dto/application/response/CancelApplicationResponse.java
+++ backend/src/main/java/io/github/crewhub/dto/application/response/CancelApplicationResponse.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
  * 지원 취소 응답 데이터
  */
 @Builder
-public record CancleApplicationResponse(
+public record CancelApplicationResponse(
         Integer applicationId,
         Integer gatheringId,
         ApplicationStatus status,
