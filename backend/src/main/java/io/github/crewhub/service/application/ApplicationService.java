0325282aa629a936f9d1947ed3b15e07e2d0cf2a
ase10git <ase1031@naver.com>
2026-06-22 05:55:19 +0900

    feat: ApplicationService에 지원 메서드 추가(#8)

---
+++ backend/src/main/java/io/github/crewhub/service/application/ApplicationService.java
@@ -0,0 +1,87 @@
+package io.github.crewhub.service.application;
+
+import io.github.crewhub.common.exception.BusinessException;
+import io.github.crewhub.dto.application.request.CreateApplicationRequest;
+import io.github.crewhub.dto.application.response.CreateApplicationResponse;
+import io.github.crewhub.entity.application.Application;
+import io.github.crewhub.entity.gathering.Gathering;
+import io.github.crewhub.entity.gathering.GatheringMemberId;
+import io.github.crewhub.entity.user.User;
+import io.github.crewhub.enums.application.ApplicationStatus;
+import io.github.crewhub.enums.common.ErrorCode;
+import io.github.crewhub.repository.application.ApplicationRepository;
+import io.github.crewhub.repository.gathering.GatheringMemberRepository;
+import io.github.crewhub.repository.gathering.GatheringRepository;
+import io.github.crewhub.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 지원서 서비스
+ */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ApplicationService {
+    private final UserRepository userRepository;
+    private final ApplicationRepository applicationRepository;
+    private final GatheringRepository gatheringRepository;
+    private final GatheringMemberRepository memberRepository;
+
+    @Transactional
+    public CreateApplicationResponse apply(
+            Integer userId,
+            CreateApplicationRequest request
+    ) {
+        User user = getUser(userId);
+
+        Gathering gathering = getGathering(request.gatheringId());
+
+        validateDuplicateApplication(userId, gathering.getId());
+
+        validateAlreadyMember(userId, gathering.getId());
+
+        Application application = Application.builder()
+                        .user(user)
+                        .gathering(gathering)
+                        .status(ApplicationStatus.PENDING)
+                        .build();
+
+        Application saved = applicationRepository.save(application);
+
+        return CreateApplicationResponse.builder()
+                .applicationId(saved.getId())
+                .gatheringId(gathering.getId())
+                .gatheringName(gathering.getGatheringName())
+                .status(saved.getStatus())
+                .createdAt(saved.getCreatedAt())
+                .build();
+    }
+
+    private User getUser(Integer userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    private Gathering getGathering(Integer gatheringId) {
+        return gatheringRepository.findById(gatheringId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.GATHERING_NOT_FOUND));
+    }
+
+    private void validateDuplicateApplication(Integer userId, Integer gatheringId) {
+        if (applicationRepository.existsByUserIdAndGatheringId(userId, gatheringId)) {
+            throw new BusinessException(ErrorCode.DUPLICATE_APPLICATION);
+        }
+    }
+
+    private void validateAlreadyMember(Integer userId, Integer gatheringId) {
+        GatheringMemberId memberId =
+                new GatheringMemberId(gatheringId, userId);
+
+        if (memberRepository.existsById(memberId)) {
+            throw new BusinessException(ErrorCode.DUPLICATE_GATHERING_MEMBER);
+        }
+    }
+
+}
