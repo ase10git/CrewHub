97f1c0c46a81d4c31c8273ad7ca773a9e4663c00
ase10git <ase1031@naver.com>
2026-06-22 07:21:54 +0900

    feat: 지원서 단일 조회 메서드 추가(#8)

--- backend/src/main/java/io/github/crewhub/repository/application/ApplicationRepository.java
+++ backend/src/main/java/io/github/crewhub/repository/application/ApplicationRepository.java
@@ -8,11 +8,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 /**
  * 지원서 Entity 관리용 Repository
  */
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
     boolean existsByUserIdAndGatheringId(Integer userId, Integer gatheringId);
+    Optional<Application> findByIdAndUserId(Integer applicationId, Integer userId);
     @Query(
     value = """
         select a
