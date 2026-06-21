9cce5b4a7574903f5d6ebb3eefa6b4e521bef394
ase10git <ase1031@naver.com>
2026-06-22 05:55:05 +0900

    feat: ApplicationRepository에 중복 지원서 탐색 메서드 추가(#8)

--- backend/src/main/java/io/github/crewhub/repository/application/ApplicationRepository.java
+++ backend/src/main/java/io/github/crewhub/repository/application/ApplicationRepository.java
@@ -1,10 +1,17 @@
 package io.github.crewhub.repository.application;
 
 import io.github.crewhub.entity.application.Application;
+import io.github.crewhub.enums.application.ApplicationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * 지원서 Entity 관리용 Repository
  */
 public interface ApplicationRepository extends JpaRepository<Application, Integer> {
+    boolean existsByUserIdAndGatheringId(Integer userId, Integer gatheringId);
+    boolean existsByUserIdAndGatheringIdAndStatus(
+            Integer userId,
+            Integer gatheringId,
+            ApplicationStatus status
+    );
 }
