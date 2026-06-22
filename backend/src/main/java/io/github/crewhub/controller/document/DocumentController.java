23942737116afc1833ad59569814a342d7211667
ase10git <ase1031@naver.com>
2026-06-22 14:23:59 +0900

    feat: 문서 작성 API 추가(#9)

--- backend/src/main/java/io/github/crewhub/controller/document/DocumentController.java
+++ backend/src/main/java/io/github/crewhub/controller/document/DocumentController.java
@@ -1,6 +1,15 @@
 package io.github.crewhub.controller.document;
 
+import io.github.crewhub.common.response.ApiResponse;
+import io.github.crewhub.dto.document.request.CreateDocumentRequest;
+import io.github.crewhub.dto.document.response.CreateDocumentResponse;
+import io.github.crewhub.security.details.CustomUserDetails;
+import io.github.crewhub.service.document.DocumentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/document")
 @RequiredArgsConstructor
 public class DocumentController {
+    private final DocumentService documentService;
+
+    @PostMapping
+    public ApiResponse<CreateDocumentResponse> create(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @Valid @RequestBody CreateDocumentRequest request
+    ) {
+
+        return ApiResponse.success(
+                documentService.create(userDetails.getUserId(), request)
+        );
+    }
 }
