dea40f8e6d90b64911269a0713c55d6e0545df75
ase10git <ase1031@naver.com>
2026-06-22 17:45:54 +0900

    feat: 문서 수정 관련 에러 코드 추가(#9)

--- backend/src/main/java/io/github/crewhub/enums/common/ErrorCode.java
+++ backend/src/main/java/io/github/crewhub/enums/common/ErrorCode.java
@@ -35,6 +35,7 @@ public enum ErrorCode {
     GATHERING_MANAGER_ONLY(HttpStatus.FORBIDDEN, "모임 관리자만 가능합니다."),
     MANAGER_CANNOT_LEAVE(HttpStatus.FORBIDDEN, "모임 관리자는 탈퇴할 수 없습니다. 먼저 관리자 권한을 위임하세요."),
     GATHERING_MEMBER_ONLY(HttpStatus.FORBIDDEN, "모임 회원만 이용할 수 있습니다."),
+    WRITER_ONLY(HttpStatus.FORBIDDEN, "작성자만 이용할 수 있습니다."),
 
     INTERNAL_SERVER_ERROR(
             HttpStatus.INTERNAL_SERVER_ERROR,
