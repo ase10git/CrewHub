113873988432a1e47e8982aeaa65d2ee2c196920
ase10git <ase1031@naver.com>
2026-06-22 09:41:51 +0900

    feat: 모임 회원 조회 Repository 메서드 변경(역할 필터링 추가), 회원 관리를 위한 회원 검색 메서드 추가(#16)

--- backend/src/main/java/io/github/crewhub/repository/gathering/GatheringMemberRepository.java
+++ backend/src/main/java/io/github/crewhub/repository/gathering/GatheringMemberRepository.java
@@ -2,11 +2,15 @@ package io.github.crewhub.repository.gathering;
 
 import io.github.crewhub.entity.gathering.GatheringMember;
 import io.github.crewhub.entity.gathering.GatheringMemberId;
+import io.github.crewhub.enums.gathering.MemberRole;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * 모임 가입자 Entity 관리용 Repository
@@ -14,18 +18,27 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GatheringMemberRepository extends JpaRepository<GatheringMember, GatheringMemberId> {
     boolean existsByGatheringIdAndUserId(Integer gatheringId, Integer userId);
-    @Query(
-    value = """
+    @Query("""
+        select gm
+        from GatheringMember gm
+        where gm.gathering.id = :gatheringId
+        and gm.user.id = :userId
+    """)
+    Optional<GatheringMember> findMember(
+            @Param("gatheringId") Integer gatheringId,
+            @Param("userId") Integer userId
+    );
+    Page<GatheringMember> findByUserId(Integer userId, Pageable pageable);
+    @Query("""
         select gm
         from GatheringMember gm
         join fetch gm.user
         where gm.gathering.id = :gatheringId
-    """,
-    countQuery = """
-        select count(gm)
-        from GatheringMember gm
-        where gm.gathering.id = :gatheringId
-    """
-    )
-    Page<GatheringMember> findByGatheringId(Integer gatheringId, Pageable pageable);
+        and (:role is null or gm.role = :role)
+    """)
+    Page<GatheringMember> findMembers(
+            @Param("gatheringId") Integer gatheringId,
+            @Param("role") MemberRole role,
+            Pageable pageable
+    );
 }
