bce60c81ebc73af7f2958175d4f12b7e441a7417
ase10git <ase1031@naver.com>
2026-06-22 15:24:49 +0900

    feat: 모임별 문서 목록 조회 API 추가(#9)

--- backend/src/main/java/io/github/crewhub/controller/document/DocumentController.java
+++ backend/src/main/java/io/github/crewhub/controller/document/DocumentController.java
@@ -1,28 +1,27 @@
 package io.github.crewhub.controller.document;
 
 import io.github.crewhub.common.response.ApiResponse;
+import io.github.crewhub.dto.common.PageResponse;
 import io.github.crewhub.dto.document.request.CreateDocumentRequest;
 import io.github.crewhub.dto.document.response.CreateDocumentResponse;
+import io.github.crewhub.dto.document.response.DocumentSummaryResponse;
 import io.github.crewhub.security.details.CustomUserDetails;
 import io.github.crewhub.service.document.DocumentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 모임 정보 요청 처리
  */
 @RestController
-@RequestMapping("/api/document")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class DocumentController {
     private final DocumentService documentService;
 
-    @PostMapping
+    @PostMapping("/gathering/document")
     public ApiResponse<CreateDocumentResponse> create(
             @AuthenticationPrincipal CustomUserDetails userDetails,
             @Valid @RequestBody CreateDocumentRequest request
@@ -30,6 +29,23 @@ public class DocumentController {
 
         return ApiResponse.success(
                 documentService.create(userDetails.getUserId(), request)
+        );
+    }
+
+    @GetMapping("/gathering/{gatheringId}/document")
+    public ApiResponse<PageResponse<DocumentSummaryResponse>> getDocuments(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Integer gatheringId,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        return ApiResponse.success(
+                documentService.getDocuments(
+                        userDetails.getUserId(),
+                        gatheringId,
+                        page,
+                        size
+                )
         );
     }
 }
