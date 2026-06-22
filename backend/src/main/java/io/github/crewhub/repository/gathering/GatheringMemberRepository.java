b3f0bbfda88799dc0c966f139273a7b76cbfd81a
ase10git <ase1031@naver.com>
2026-06-22 09:31:13 +0900

    feat: 모임 회원 조회용 Repository 메서드 추가(#16)

--- backend/src/main/java/io/github/crewhub/repository/gathering/GatheringMemberRepository.java
+++ backend/src/main/java/io/github/crewhub/repository/gathering/GatheringMemberRepository.java
@@ -2,7 +2,10 @@ package io.github.crewhub.repository.gathering;
 
 import io.github.crewhub.entity.gathering.GatheringMember;
 import io.github.crewhub.entity.gathering.GatheringMemberId;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -11,4 +14,18 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface GatheringMemberRepository extends JpaRepository<GatheringMember, GatheringMemberId> {
     boolean existsByGatheringIdAndUserId(Integer gatheringId, Integer userId);
+    @Query(
+    value = """
+        select gm
+        from GatheringMember gm
+        join fetch gm.user
+        where gm.gathering.id = :gatheringId
+    """,
+    countQuery = """
+        select count(gm)
+        from GatheringMember gm
+        where gm.gathering.id = :gatheringId
+    """
+    )
+    Page<GatheringMember> findByGatheringId(Integer gatheringId, Pageable pageable);
 }
