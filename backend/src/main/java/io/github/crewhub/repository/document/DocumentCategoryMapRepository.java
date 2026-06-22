6b68c5f601b6d345ce0a432d72489a24377c8c2c
ase10git <ase1031@naver.com>
2026-06-22 16:12:11 +0900

    feat: 문서 상세 조회 및 문서 카테고리 목록 조회 Repository 메서드와 쿼리 추가(#9)

--- backend/src/main/java/io/github/crewhub/repository/document/DocumentCategoryMapRepository.java
+++ backend/src/main/java/io/github/crewhub/repository/document/DocumentCategoryMapRepository.java
@@ -3,11 +3,22 @@ package io.github.crewhub.repository.document;
 import io.github.crewhub.entity.document.DocumentCategoryMap;
 import io.github.crewhub.entity.document.DocumentCategoryMapId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * 문서 카테고리 Map Entity 관리용 Repository
  */
 @Repository
 public interface DocumentCategoryMapRepository extends JpaRepository<DocumentCategoryMap, DocumentCategoryMapId> {
+    @Query("""
+        select dcm
+        from DocumentCategoryMap dcm
+        join fetch dcm.category
+        where dcm.document.id = :documentId
+    """)
+    List<DocumentCategoryMap> findDocumentCategory(@Param("documentId") Integer documentId);
 }
--- backend/src/main/java/io/github/crewhub/repository/document/DocumentRepository.java
+++ backend/src/main/java/io/github/crewhub/repository/document/DocumentRepository.java
@@ -4,7 +4,11 @@ import io.github.crewhub.entity.document.Document;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * 문서 Entity 관리용 Repository
@@ -12,4 +16,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Integer> {
     Page<Document> findByGatheringIdAndIsDeletedFalse(Integer gatheringId, Pageable pageable);
+    @Query(
+    value = """
+        select d
+        from Document d
+        join fetch d.writer
+        join fetch d.gathering
+        where d.isDeleted = false
+        and d.id = :documentId
+    """
+    )
+    Optional<Document> findDetailById(@Param("documentId") Integer documentId);
 }
