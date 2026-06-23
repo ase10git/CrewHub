2f3227d404af2c567559fca668b4ce10004449cb
ase10git <ase1031@naver.com>
2026-06-24 08:57:32 +0900

    refactor: 추가 쿼리 방지를 위한 채팅방 조회 쿼리 추가(#10)

--- backend/src/main/java/io/github/crewhub/repository/chat/ChatRoomRepository.java
+++ backend/src/main/java/io/github/crewhub/repository/chat/ChatRoomRepository.java
@@ -2,11 +2,23 @@ package io.github.crewhub.repository.chat;
 
 import io.github.crewhub.entity.chat.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * 채팅방 Entity 관리용 Repository
  */
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Integer> {
+    @Query("""
+        select cr
+        from ChatRoom cr
+        join fetch cr.gathering
+        where cr.id = :roomId
+    """)
+    Optional<ChatRoom> findById(@Param("roomId") Integer roomId);
+    Optional<ChatRoom> findByGatheringId(Integer gatheringId);
 }
