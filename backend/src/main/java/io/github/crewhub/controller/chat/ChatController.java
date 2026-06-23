947c26f7f5923ed9674ed337a271c3bec5c7f87f
ase10git <ase1031@naver.com>
2026-06-23 22:55:29 +0900

    feat: 채팅방 조회 API 추가(#10)

--- backend/src/main/java/io/github/crewhub/controller/chat/ChatController.java
+++ backend/src/main/java/io/github/crewhub/controller/chat/ChatController.java
@@ -1,8 +1,14 @@
 package io.github.crewhub.controller.chat;
 
 
+import io.github.crewhub.common.response.ApiResponse;
+import io.github.crewhub.dto.chat.response.ChatRoomResponse;
+import io.github.crewhub.security.details.CustomUserDetails;
 import io.github.crewhub.service.chat.ChatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +20,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ChatController {
     private final ChatService chatService;
+
+    @GetMapping("/room/{roomId}")
+    public ApiResponse<ChatRoomResponse> getRoom(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Integer roomId
+    ) {
+        return ApiResponse.success(
+                chatService.getRoom(userDetails.getUserId(), roomId)
+        );
+    }
 }
