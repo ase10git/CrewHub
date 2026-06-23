ddc5f36316a350c38e5d5bc2b3f521a4e7e0ade0
ase10git <ase1031@naver.com>
2026-06-23 22:55:17 +0900

    feat: 채팅방 조회 Service 메서드 추가(#10)

--- backend/src/main/java/io/github/crewhub/service/chat/ChatService.java
+++ backend/src/main/java/io/github/crewhub/service/chat/ChatService.java
@@ -1,7 +1,12 @@
 package io.github.crewhub.service.chat;
 
+import io.github.crewhub.common.exception.BusinessException;
+import io.github.crewhub.dto.chat.response.ChatRoomResponse;
+import io.github.crewhub.entity.chat.ChatRoom;
+import io.github.crewhub.enums.common.ErrorCode;
 import io.github.crewhub.repository.chat.ChatMessageRepository;
 import io.github.crewhub.repository.chat.ChatRoomRepository;
+import io.github.crewhub.repository.gathering.GatheringMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,4 +20,36 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageRepository chatMessageRepository;
+    private final GatheringMemberRepository memberRepository;
+
+    public ChatRoomResponse getRoom(Integer userId, Integer roomId) {
+        ChatRoom room = chatRoomRepository.findById(roomId)
+                .orElseThrow(
+                        () -> new BusinessException(ErrorCode.CHATROOM_NOT_FOUND)
+                );
+
+        validateMember(userId, room.getGathering().getId());
+
+        return ChatRoomResponse.builder()
+                .roomId(roomId)
+                .gatheringId(room.getGathering().getId())
+                .createdAt(room.getCreatedAt())
+                .updatedAt(room.getUpdatedAt())
+                .build();
+    }
+
+    private void validateMember(Integer userId, Integer gatheringId) {
+        boolean exists = memberRepository.existsByGatheringIdAndUserId(gatheringId, userId);
+
+        if (!exists) {
+            throw new BusinessException(ErrorCode.GATHERING_MEMBER_ONLY);
+        }
+    }
+
+    private ChatRoom findRoom(Integer roomId) {
+        return chatRoomRepository.findById(roomId)
+                .orElseThrow(
+                        () -> new BusinessException(ErrorCode.CHATROOM_NOT_FOUND)
+                );
+    }
 }
