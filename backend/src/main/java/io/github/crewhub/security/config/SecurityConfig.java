8c2ccbd5f331849c2f208cbd9345f2f92f75e8fe
ase10git <ase1031@naver.com>
2026-06-24 12:40:17 +0900

    feat: 프론트와의 통신을 위한 CORS 설정 추가(#10)

--- backend/src/main/java/io/github/crewhub/security/config/SecurityConfig.java
+++ backend/src/main/java/io/github/crewhub/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import io.github.crewhub.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 /**
  * Spring Security 설정
@@ -51,7 +57,10 @@ public class SecurityConfig {
                                 "/v1/api-docs/**",
 
                                 "/api/gatherings",
-                                "/api/gatherings/*"
+                                "/api/gatherings/*",
+
+                                "/ws/**",
+                                "/ws"
                         ).permitAll()
 
                         .anyRequest().authenticated()
@@ -62,8 +71,39 @@ public class SecurityConfig {
                 ).exceptionHandling(exception ->
                         exception.authenticationEntryPoint(
                                 jwtAuthenticationEntryPoint
-                        ));
+                        ))
+                .cors(Customizer.withDefaults())
+        ;
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowedOrigins(
+                List.of("http://localhost:5173")
+        );
+
+        config.setAllowedMethods(
+                List.of("*")
+        );
+
+        config.setAllowedHeaders(
+                List.of("*")
+        );
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration(
+                "/**",
+                config
+        );
+
+        return source;
     }
 }
