21d4f3d14531006d2d8f24b498edd07cc4c03cbf
ase10git <ase1031@naver.com>
2026-06-25 18:30:43 +0900

    chore: 프론트엔드 코드 제거(#11)

--- frontend/src/api/vibexClient.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@/sdk/index';
-export const vibex = createClient({
-  serverUrl: 'https://app.vibe-x.app/v1/71212311046d33c35ec0f71a/entities',
-  appId: '71212311046d33c35ec0f71a',
-});
