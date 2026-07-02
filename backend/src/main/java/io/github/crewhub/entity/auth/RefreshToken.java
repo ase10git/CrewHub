bed1b232a9f421305a9e9053e6a48a1f52ad29b1
ase10git <ase1031@naver.com>
2026-07-02 20:19:16 +0900

    feat: Redis 저장용 RefreshToken 클래스와 Repository 추가(#30)

---
+++ backend/src/main/java/io/github/crewhub/entity/auth/RefreshToken.java
@@ -0,0 +1,34 @@
+package io.github.crewhub.entity.auth;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import java.time.LocalDateTime;
+
+/**
+ * JWT Refresh Token
+ * Redis 저장
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@RedisHash("refresh_token")
+public class RefreshToken {
+
+    @Id
+    private Integer userId;
+
+    @Indexed
+    private String refreshTokenHash;
+
+    private LocalDateTime expiredAt;
+
+    private LocalDateTime createdAt;
+
+    @TimeToLive
+    private Long ttl;
+}
---
+++ backend/src/main/java/io/github/crewhub/repository/token/TokenRepository.java
@@ -0,0 +1,13 @@
+package io.github.crewhub.repository.token;
+
+import io.github.crewhub.entity.auth.RefreshToken;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Refresh Token 관리를 위한 Redis Repository
+ * 객체(Entity) 중심의 CRUD 관리 시 사용
+ */
+@Repository
+public interface TokenRepository extends ListCrudRepository<RefreshToken, String> {
+}
