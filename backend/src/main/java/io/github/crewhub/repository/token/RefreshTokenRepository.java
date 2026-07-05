354b3356700fb4655d681e334652e1db0227bf4e
ase10git <ase1031@naver.com>
2026-07-06 01:38:46 +0900

    feat: FamilyId로 RefreshToken 조회용 메서드 추가(#30)

--- backend/src/main/java/io/github/crewhub/repository/token/RefreshTokenRepository.java
+++ backend/src/main/java/io/github/crewhub/repository/token/RefreshTokenRepository.java
@@ -4,10 +4,13 @@ import io.github.crewhub.entity.auth.RefreshToken;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Refresh Token 관리를 위한 Redis Repository
  * 객체(Entity) 중심의 CRUD 관리 시 사용
  */
 @Repository
 public interface RefreshTokenRepository extends ListCrudRepository<RefreshToken, String> {
+    List<RefreshToken> findAllByFamilyId(String familyId);
 }
